352c4bc5bb6f2745ef9d4953871a8f9b98541b0c
tyr1kqq <igorturkinwhite@yandex.ru>
2024-01-19 11:43:01 +0200

    программа по блок схеме

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,20 +1,72 @@
 ﻿namespace Algorithm
 {
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+      
+    }
+
+   
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите свой возвраст - ");
-            int age = Int32.Parse(Console.ReadLine());
+            User user1 = new User();
+            User user2 = new User();
+            User user3 = new User();
 
-            if (age > 13)
+            user1.Login = "Makaka";
+            user1.Name = "Daniil0";
+            user1.IsPremium = false;
+
+            user2.Login = "Gora";
+            user2.Name = "Igor";
+            user2.IsPremium = true;
+
+            user3.Login = "NeeilStudio";
+            user3.Name = "Snegana";
+            user3.IsPremium = true;
+
+            Console.WriteLine("Hello" + user1.Name);
+            if (!user1.IsPremium)
             {
-                Console.WriteLine("Вы успешно авторизовались");
+                ShowAds();
             }
-            else
+            Console.WriteLine("");
+
+            Console.WriteLine("Hello" + user2.Name);
+            if (!user2.IsPremium)
             {
-                Console.WriteLine("Мудила ты еще маленький");
+                ShowAds();
             }
+
+            Console.WriteLine("");
+            Console.WriteLine("Hello"+ user3.Name);
+            if (!user3.IsPremium)
+            {
+                ShowAds();
+            }
+
+
+
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
         }
     }
 }
