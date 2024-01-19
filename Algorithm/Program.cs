7eda01b74a8e8a515558cfe42d03cbe89f51d19e
tyr1kqq <igorturkinwhite@yandex.ru>
2024-01-19 11:14:29 +0200

    Добавьте файлы проекта.

---
+++ Algorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свой возвраст - ");
+            int age = Int32.Parse(Console.ReadLine());
+
+            if (age > 13)
+            {
+                Console.WriteLine("Вы успешно авторизовались");
+            }
+            else
+            {
+                Console.WriteLine("Мудила ты еще маленький");
+            }
+        }
+    }
+}
